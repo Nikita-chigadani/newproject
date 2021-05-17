ae85ebb633c5fc243d102a3ea998cfa7088b5495
Nikita C <nikita149156@gmail.com>
2021-05-17 18:21:05 +0530

    tweets

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: "main#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "session#destroy"
+  get "about", to: "about#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
